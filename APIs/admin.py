7baf755c6351a7fffdb2afafb7d9aa1c9a479e10
amrremam <erorat10@gmail.com>
2023-08-31 17:24:30 +0300

    first init

---
+++ APIs/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import New
+
+
+# Register your models here.
+
+
+
+admin.site.register(New)
+
+
+
+
+
+admin.site.site_header = 'DMS'
+admin.site.site_title = 'DMS'
---
+++ APIs/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+
+
+
+class New(models.Model):
+    title = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
