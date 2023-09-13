9b6a8148504459f90b5f209e1ceb57c1a0c0295b
amrremam <erorat10@gmail.com>
2023-09-13 12:02:42 +0300

    add new host

--- project/settings.py
+++ project/settings.py
@@ -18,7 +18,7 @@ DEBUG  = config('DEBUG')
 # DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1','.localhost', ]
+ALLOWED_HOSTS = ['127.0.0.1','.localhost', 'kayanapp-738a5bdedd9c.herokuapp.com']
 
 
 # Application definition
