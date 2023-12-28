f08115e29432d5db8a02c61a59cfc32787e26e38
amrremam <erorat10@gmail.com>
2023-12-28 15:26:26 +0200

    eroritoo

--- project/settings.py
+++ project/settings.py
@@ -1,31 +1,20 @@
-from pathlib import Path
 from decouple import config
 import os
+from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-
-# SECRET_KEY = 'django-insecure-ygw1s2mom#po=mlt_pmz!xz#$j_-etxb7)u*br^$u%8gjcfad!'
 SECRET_KEY = config('SECRET_KEY')
 
-
-DEBUG  = config('DEBUG')
-# DEBUG = True
-
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1','.localhost', 'dms.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://dms.up.railway.app']
 
-# Application definition
 
 INSTALLED_APPS = [
     'jazzmin',
-    'APIs',
     'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -33,13 +22,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'APIs',
     'rest_framework',
 ]
-
-
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -53,7 +38,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'project.urls'
 
 CORS_ALLOWED_ORIGINS = [
         "https://dms.up.railway.app",
@@ -63,6 +47,9 @@ CORS_ALLOWED_ORIGINS = [
         "http://127.0.0.1:9000"
     ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -80,31 +67,16 @@ TEMPLATES = [
     },
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
-
 WSGI_APPLICATION = 'project.wsgi.application'
 
 
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "railway",
-        "USER": "postgres",
-        "PASSWORD": "4eDc*eD6a6CFfCcECGFE22b1CB1gD2GB",
-        "HOST": "viaduct.proxy.rlwy.net",
-        "PORT": "55702",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-
-
-
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -122,9 +94,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -134,17 +103,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 
 JAZZMIN_SETTINGS = {
